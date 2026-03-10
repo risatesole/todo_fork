33edd92d094b40bfc18aa57937f0810a7e6bf6c6
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-03-09 23:33:28 -0400

    feat(signup): add static signup page skeleton

--- app/(account)/signup/page.tsx
+++ app/(account)/signup/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import env from "@/config/environment";
+import TextField from "@/components/textfield";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>{env.APPNAME}</h1>
-            <p>SignUp page</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>{env.APPNAME}</h1>
+      <p>SignUp page</p>
+
+      <TextField labelText="Name" placeholderText="Enter your name" />
+      <TextField labelText="Email" placeholderText="Enter your email" fieldType="email" />
+      <TextField labelText="Password" placeholderText="Enter your password" fieldType="password" />
+      <TextField labelText="Password" placeholderText="Enter your password once more" fieldType="password" />
+
+      <button>Sign Up</button>
+    </div>
+  );
 }
