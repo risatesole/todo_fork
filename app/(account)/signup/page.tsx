c492b1d233f33a22d1db9e24b13602a78df8a4f5
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-03-12 00:13:16 -0400

    feat: update signup page to use authentication service, updated authentication service to use localstorage, removed utility localstorage class and place it in authentication service

--- app/(account)/signup/page.tsx
+++ app/(account)/signup/page.tsx
@@ -1,20 +1,98 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
 import env from "@/config/environment";
 import TextField from "@/components/textfield";
+import AuthenticationService from "@/services/authentication";
 
-export default function SignUpPage() {
+import { useRouter } from "next/navigation";
+
+async function handleSignup(
+  auth: AuthenticationService,
+  router: any,
+  name: string,
+  password: string,
+  confirmPassword: string,
+) {
+  if (!name) {
+    alert("Please fill all fields");
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match");
+    return;
+  }
+
+  try {
+    const result = await auth.signup(name);
+
+    if (result.success === false) {
+      throw new Error("signup failed");
+    }
+
+    router.push("/tasks");
+  } catch (error) {
+    alert("Signup failed");
+  }
+}
+
+export default function Page() {
+  const auth = new AuthenticationService("local");
+  const router = useRouter();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  useEffect(() => {
+    // This function runs once when the component mounts (page loads)
+    const checkbrowserToken = auth.getBrowserStorgedToken();
+    if (checkbrowserToken) {
+      router.push("/tasks"); // redirect to tasks
+    }
+  }, []);
+
   return (
     <div>
       <h1>{env.APPNAME}</h1>
       <p>SignUp page</p>
 
-      <TextField labelText="Name" placeholderText="Enter your name" />
-      <TextField labelText="Email" placeholderText="Enter your email" fieldType="email" />
-      <TextField labelText="Password" placeholderText="Enter your password" fieldType="password" />
-      <TextField labelText="Password" placeholderText="Enter your password once more" fieldType="password" />
+      <TextField
+        labelText="Name"
+        placeholderText="Enter your name"
+        onValueChange={setName}
+      />
 
-      <button>Sign Up</button>
+      <TextField
+        labelText="Email"
+        placeholderText="EnterYourEmail"
+        onValueChange={setEmail}
+      />
+
+      <TextField
+        labelText="Password"
+        placeholderText="Enter your password"
+        onValueChange={setPassword}
+        fieldType="password"
+      />
+
+      <TextField
+        labelText="Confirm Password"
+        placeholderText="Confirm your password"
+        onValueChange={setConfirmPassword}
+        fieldType="password"
+      />
+
+      <button
+        onClick={() =>
+          handleSignup(auth, router, name, password, confirmPassword)
+        }
+      >
+        Sign Up
+      </button>
     </div>
   );
 }
--- services/authentication/index.ts
+++ services/authentication/index.ts
@@ -1,16 +1,70 @@
+class LocalStorage {
+  setValue(key: string, value: any) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  getValue(key: string) {
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  }
+
+  removeValue(key: string) {
+    localStorage.removeItem(key);
+  }
+}
+
 export default class AuthenticationService {
   private baseUrl: any;
+  private browserStorage;
 
   constructor(baseUrl: string) {
+    // variables used:
+    //token
+
     this.baseUrl = baseUrl;
+    this.browserStorage = new LocalStorage();
+  }
+
+  private setBrowserStorgedToken(token: string) {
+    this.browserStorage.setValue("token", token);
+  }
+
+  getBrowserStorgedToken() {
+    // this method shows what session is active
+
+    return this.browserStorage.getValue("token");
+
+    // throw new Error("Method not implemented");
   }
 
   async signIn(email: string, password: string) {
     throw new Error("Method not implemented yet");
   }
 
-  async signup(name: string, email: string, password: string) {
-    throw new Error("Method not implemented yet");
+  async signup(name: string) {
+    // check if there are active user sessions
+    // create a user with a session id
+
+    // throw error if there is a active user session
+    // throw error if user alreddy exists, in this case the existing user is duck
+
+    const tokenStorgedInBrowser = this.getBrowserStorgedToken();
+
+    // console.log(tokenStorgedInBrowser)
+
+    if (tokenStorgedInBrowser !== null) {
+      // console.log("there is a user session active");
+      return {
+        success: false,
+        message: "there is alreddy a active token in browser",
+      };
+    }
+
+    this.setBrowserStorgedToken("token");
+    return {
+      success: true,
+      message: "user signed up successfully",
+    };
   }
 
   async signOut(accessToken: string) {
